c90075c68fab7266105676276d2cbafb020d9987
Rohit Yadav <131008606+rohit-yadav-4099@users.noreply.github.com>
2023-10-23 22:17:47 +0530

    Update Link.js

--- src/ContextAPI/Link.js
+++ src/ContextAPI/Link.js
@@ -19,49 +19,49 @@ function RoutingCom() {
 
     return (
         <>
-                <div className='hambuger-title'>
-                    <div className='head'>
-                        <span className='The'>The</span>
-                        <h1 className='Siren'> Siren</h1>
-                    </div>
-                    <div className='hamburger'>
+            <div className='hambuger-title'>
+                <div className='head'>
+                    <span className='The'>The</span>
+                    <h1 className='Siren'> Siren</h1>
+                </div>
+                <div className='hamburger'>
 
-                        <button className='hamburgerbtn' onClick={() => setisHamburger(!isHamburger)}>
-                            ___ ___ ___
-                        </button>
-
-                    </div>
+                    <button className='hamburgerbtn' onClick={() => setisHamburger(!isHamburger)}>
+                        ___ ___ ___
+                    </button>
 
                 </div>
-                <div className='navbar'>
 
-                    <BrowserRouter>
-                        <div className='nav'>
-                            <div className='main-nav'>
-                                <ul type='none' className={isHamburger ? "hamburgerbtn" : "nav"} onClick={() => setisHamburger(false)}>
-                                    <li><NavLink to="/" className='navtxt' onClick={closenavigate}>Home</NavLink></li>
-                                    <li> <NavLink to="/Bollywood" className='navtxt' onClick={closenavigate}>Bollywood</NavLink></li>
-                                    <li><NavLink to="/Technology" className='navtxt' onClick={closenavigate}>Technology</NavLink></li>
-                                    <li> <NavLink to="/Hollywood" className='navtxt' onClick={closenavigate}>Hollywood</NavLink></li>
-                                    <li><NavLink to="/Fitness" className='navtxt' onClick={closenavigate}>Fitness</NavLink></li>
-                                    <li><NavLink to="/Food" className='navtxt' onClick={closenavigate}>Food</NavLink></li>
-                                </ul>
-                            </div>
+            </div>
+            <div className='navbar'>
+
+                <BrowserRouter>
+                    <div className='nav'>
+                        <div className='main-nav'>
+                            <ul type='none' className={isHamburger ? "hamburgerbtn" : "nav"} onClick={() => setisHamburger(false)}>
+                                <li><NavLink to="/" className='navtxt' onClick={closenavigate}>Home</NavLink></li>
+                                <li> <NavLink to="/Bollywood" className='navtxt' onClick={closenavigate}>Bollywood</NavLink></li>
+                                <li><NavLink to="/Technology" className='navtxt' onClick={closenavigate}>Technology</NavLink></li>
+                                <li> <NavLink to="/Hollywood" className='navtxt' onClick={closenavigate}>Hollywood</NavLink></li>
+                                <li><NavLink to="/Fitness" className='navtxt' onClick={closenavigate}>Fitness</NavLink></li>
+                                <li><NavLink to="/Food" className='navtxt' onClick={closenavigate}>Food</NavLink></li>
+                            </ul>
                         </div>
-                        <hr />
-                        <ConntextStore>
-                            <Routes>
-                                <Route path='/' element={<Homecom />} />
-                                <Route path='/Bollywood' element={<Bollywood />} />
-                                <Route path='/Hollywood' element={<Hollywood />} />
-                                <Route path='/Food' element={<FoodCom />} />
-                                <Route path='/Technology' element={<Technology />} />
-                                <Route path='/Fitness' element={<Fitness />} />
-                                <Route path="/Navigate/:id" element={<Navigate />} />
-                            </Routes>
-                        </ConntextStore>
-                    </BrowserRouter>
-                </div>
+                    </div>
+                    <hr />
+                    <ConntextStore>
+                        <Routes>
+                            <Route path='/' element={<Homecom />} />
+                            <Route path='/Bollywood' element={<Bollywood />} />
+                            <Route path='/Hollywood' element={<Hollywood />} />
+                            <Route path='/Food' element={<FoodCom />} />
+                            <Route path='/Technology' element={<Technology />} />
+                            <Route path='/Fitness' element={<Fitness />} />
+                            <Route path="/Navigate/:id" element={<Navigate />} />
+                        </Routes>
+                    </ConntextStore>
+                </BrowserRouter>
+            </div>
             <Footer></Footer>
         </>
     );
